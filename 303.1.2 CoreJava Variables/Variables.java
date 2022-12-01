6589ef6b2dfe2f90e444b208a09cbfe2bc2bbab3
Li Chen <lichen4262474@gmail.com>
2022-11-30 20:10:05 -0600

    change format

--- 303.1.2 CoreJava Variables/Variables.java
+++ 303.1.2 CoreJava Variables/Variables.java
@@ -1,33 +1,53 @@
 package com.perscholas.java_basics;
 
-public class JavaaBasicsClass {
+public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		question 1();
+		question 2();
+		question 3();
+		question 4();
+		question 5();
+		question 6();
+		question 7();
+		question 8();
+	}
+	
+	public static void question 1() {
 		// assignment # 1
 		int a = 5;
 		int b = 6;
 		int c;
 		c = a + b;
 		System.out.println(c);
+	}
+	public static void question 2() { 
 		// assignment #2
 		double d = 5d;
 		double e = 6d;
 		double f;
 		f = d + e;
 		System.out.println(f);
+	}
+	public static void question 3() { 
 		// assignment # 3
 		double g;
 		g = a + d;
 		System.out.println(g);
-		// assignment # 4
+	}
+	public static void question 4() { 
+	// assignment # 4
 		double h;
 		h = e / d;
 		System.out.println(h);
-		// assignment # 5
+	}
+	public static void question 5() { 
+	// assignment # 5
 		double i;
 		i = e / d;
 		System.out.println((int) i);
+		}
+	public static void question 6() {
 		// assignment #6
 		int x = 5;
 		int y = 6;
@@ -35,10 +55,14 @@ public class JavaaBasicsClass {
 		System.out.println(q);
 		q = y;
 		System.out.println(q);
+	}
+	public static void question 7() { 
 		// assignment #7
 		final int J = 12;
 		int k = 9 * J;
 		System.out.println(k);
+	}
+	public static void question 8() { 
 		// assignment #8
 		float coffee = 3.2f;
 		float tea = 2.3f;
@@ -49,11 +73,11 @@ public class JavaaBasicsClass {
 		subTotal = 3 * coffee + 4 * tea + 2 * hotChocholate;
 		totalSale = subTotal * (1 + SALES_TAX);
 		System.out.printf("The totalsale is %.2f", totalSale);
+	}
 		/*
 		 * int myFirstInt = 5; int mySecondInt = 6; int max = myFirstInt > mySecondInt ?
 		 * myFirstInt : mySecondInt; System.out.println(max); String myString = "6"; int
 		 * myInt = Integer.parseInt(myString); System.out.println(myInt);
 		 */
-	}
 
-}
+}//close class
--- 303.1.3 Operators And Numbers/OperatorsAndNumbers.java
+++ 303.1.3 Operators And Numbers/OperatorsAndNumbers.java
@@ -2,10 +2,23 @@
 public class OperatorsAndNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Write a program that declares an integer a variable x and assigns the value 2 to it and prints out the binary string version of the number ( Integer.toBinaryString(x) ). Now, use the left shift operator (<<) to shift by 1 and assign the result to x. Before printing the results, write a comment with the predicted decimal value and binary string. Now, print out x in decimal form and in binary notation.
-		System.out.println("Problem 1 solution: 1, 1000, 1111, 1001110, 1100010111, 111111010000011");
-		System.out.println("Problem 2 solution: 2, 9, 52, 114, 543, 11367");
+	question1();
+	question2();
+	question3();
+	question4();
+	question5();
+	question6();
+	question7();
+		}//close main
+	
+	//Write a program that declares an integer a variable x and assigns the value 2 to it and prints out the binary string version of the number ( Integer.toBinaryString(x) ). Now, use the left shift operator (<<) to shift by 1 and assign the result to x. Before printing the results, write a comment with the predicted decimal value and binary string. Now, print out x in decimal form and in binary notation.
+	public void question1() {	
+	System.out.println("Problem 1 solution: 1, 1000, 1111, 1001110, 1100010111, 111111010000011");
+	}
+	public void question2 () {
+	System.out.println("Problem 2 solution: 2, 9, 52, 114, 543, 11367");
+	}
+	public void question3() {
 		System.out.println("Problem 3 solution: ");
 		int x = 88 << 1;
 		//predicted result: 88 is 1011000 in binary  --> 1010000
@@ -18,6 +31,8 @@ public class OperatorsAndNumbers {
 		int y = 150 >> 2;
 		//predicted result: 150 is 10010110 in binary -->100101
 		System.out.println(Integer.toBinaryString(y));
+	}
+	public void question4() {
 		// Write a program that declares three int variables x, y, and z. Assign 7 to x
 		// and 17 to y. Write a comment that indicates what you predict will be the
 		// result of the bitwise & operation on x and y. Now use the bitwise & operator
@@ -35,6 +50,8 @@ public class OperatorsAndNumbers {
 		System.out.println(c);
 		c = a | b;
 		System.out.println(c);
+	}
+	public void question5() {
 		// Write a program that declares an integer variable, assigns a number, and uses
 		// a postfix increment operator to increase the value. Print the value before
 		// and after the increment operator.
@@ -42,6 +59,8 @@ public class OperatorsAndNumbers {
 		int myInt = 90;
 		myInt++;
 		System.out.println(myInt);
+	}
+	public void question6() {
 		// Write a program that demonstrates at least 3 ways to increment a variable by
 		// 1 and does this multiple times. Assign a value to an integer variable, print
 		// it, increment by 1, print it again, increment by 1, and then print it again.
@@ -53,6 +72,7 @@ public class OperatorsAndNumbers {
 		System.out.println(v);
 		v = v + 1;
 		System.out.println(v);
+	}
 		// Write a program that declares 2 integer variables, x, and y, and then assigns
 		// 5 to x and 8 to y. Create another variable sum and assign the value of ++x
 		// added to y, and print the result. Notice the value of the sum (should be 14).
@@ -60,12 +80,12 @@ public class OperatorsAndNumbers {
 		// Notice what the value of the sum is. The first configuration incremented x
 		// and then calculated the sum, while the second configuration calculated the
 		// sum and then incremented x.
-		System.out.println("Problem 7 solution: ");
+	public void question7(){	
+	System.out.println("Problem 7 solution: ");
 		int q = 5;
 		int w = 8;
 		int sum = ++q + w;
 		System.out.println(sum);
-
-	}// close main
-
+	}
+	
 }// close class
